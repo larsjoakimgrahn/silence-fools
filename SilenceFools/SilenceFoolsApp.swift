14b554a7e4011f9205d9ac0bd66d7ccb81b4a0de
Joakim Grahn <joakim.grahn@qapital.com>
2023-03-27 18:17:05 +0200

    Initial Commit

---
+++ SilenceFools/SilenceFoolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SilenceFoolsApp.swift
+//  SilenceFools
+//
+//  Created by Joakim Grahn on 2023-03-27.
+//
+
+import SwiftUI
+
+@main
+struct SilenceFoolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
